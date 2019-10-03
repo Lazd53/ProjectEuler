de632900ded569e55eaf9e27d40ad6a62563f2df
Alex Love <lazd53@gmail.com>
2019-10-03 13:25:38 -0700

    complete euler 5

--- Proj Euler 5 -uncompleted/main.js
+++ Proj Euler 5 -uncompleted/main.js
@@ -1,48 +1,80 @@
-let title = $("title");
-let name = $("#name");
-let prompt = $("#propmt");
-let answer = $("#answer");
+// let title = $("title");
+// let name = $("#name");
+// let prompt = $("#propmt");
+// let answer = $("#answer");
 
 const problemNum = 5;
 const thePrompt = "2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder. \
       What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?";
 
-let primesArray = [2];
+let arrLCMs = [];
 
-// function testPrime (x) {
-//   for (j=2; j<x; j++) {
-//     if (x%j == 0) {
-//       return false;
-//     }
-//   }
-//   return true;
-// }
-
-function allPrimes(endNum) {
-  for (j=2; j<endNum+1; j++) {
-    console.log(j);
-    if(primesArray.every(prime => endNum%prime != 0)) {
-      primesArray.push(j);
-    }
+//create all numbers from 2-20 (variable to allow for other numbers to be used)
+let createNums = function(num){
+  let allNums = [];
+  for (let i=2; i<num+1; i++ ){
+    allNums.push(i);
   }
+  return allNums;
 }
 
-allPrimes(20);
+// find LCM for each number
+function createLCM(x){
+  let arr = {};
+  let target = x;
+  for (let i=2; i<x+1;){
+    if (target%i == 0){
+      if(!arr[i]){
+        arr[i] = 1;
+      }else{
+        arr[i] = arr[i] +1;
+      }
+      target = target/i;
+    }else{
+      i++;
+    }
+  }
+  return arr
+}
 
-// for (i=0; i<20; i++) {
-//   if (testPrime(i) == true) {
-//     console.log(`${i} ${testPrime(i)}`);
-//     primesArray.push(i);
-//   }
-// };
+// Consolidate LCMs down to highest value for each number (ie 4 2s, 2 3s, 1 5, etc)
+function consolidate(arrObjs){
+  let consolidatedObj = {};
+  for (obj of arrObjs){
+    let objKeys = Object.keys(obj);
+    for (key of objKeys){
+      if (consolidatedObj[key] ==  undefined){
+        consolidatedObj[key] = obj[key];
+      }else{
+        if(consolidatedObj[key] < obj[key]){
+          consolidatedObj[key] = obj[key];
+        }
+      }
+    }
+  }
+  return consolidatedObj;
+}
 
-// console.log(primesArray);
-// console.log(testPrime(6));
-let theResults = "Here's the answer";
+//multiply it all together
+function getResult(obj){
+  let keys = Object.keys(obj);
+  let arr = [];
+  keys.map( x => arr.push(parseInt(x)**obj[x]));
+  return arr.reduce((acc, currentValue) => acc*currentValue);
+}
+
+let allNums = createNums(20);
+allNums.map( x => arrLCMs.push(createLCM(x)));
+let greatestLCMs = consolidate(arrLCMs);
 
 
 
-name.html(`Project Euler Problem ${problemNum}`);
-title.html(`Project Euler Problem ${problemNum}`);
-prompt.html(thePrompt);
-answer.html(theResults);
+let theResults = getResult(consolidate(arrLCMs));
+
+
+
+
+// name.html(`Project Euler Problem ${problemNum}`);
+// title.html(`Project Euler Problem ${problemNum}`);
+// prompt.html(thePrompt);
+// answer.html(theResults);
